f9aa51a02678fbd4c38a80e3372a046af2a90335
Renan1309 <renan.a.b.silva@gmail.com>
2020-03-13 11:47:42 -0300

    Alteracao do component login para pegar os campos digitados

--- src/component/loginComponent.js
+++ src/component/loginComponent.js
@@ -5,11 +5,38 @@ import logopitang from '../assets/logopitang.PNG'
 
 class LoginComponent extends Component{
 
-    state = {
-        email :'',
-        password : ''
+    constructor(){
+        super();
+        this.state = {
+            email :'',
+            password : '',
+           
+            
+    
+        };
+      // this.setEmail = this.setEmail.bind(this);  //pergunta a rubens o pq quando faz function normal ele pede q passe no constructor e com bind(this)
+      
 
-    };
+
+    }
+
+   
+    setEmail = event => {
+        this.setState({email:event.target.value});
+    }
+    /*
+    Modo antigo que eu estava fazendo
+    setEmail(evento){
+        this.setState({email:evento.target.value}); 
+      }
+    */
+      setPassword = event => {
+        this.setState({password:event.target.value});
+    }
+
+    submitlistener = event => {
+        alert("Form de login  Email : " + this.state.email + " Password : " + this.state.password);
+    }
 
 
 render(){
@@ -17,6 +44,9 @@ render(){
 
     return(
         <>
+
+           <p>{this.state.email}</p>
+           <p>{this.state.password}</p>
       
         <div className="container">
             <img src={logopitang}  alt="LogoPitang"></img>
@@ -26,10 +56,10 @@ render(){
 
                 <form>
                     <label >Login</label>
-                    <input type="email" id="email" placeholder="login@pitang.com"></input>
+                    <input onChange ={this.setEmail} value = {this.state.email} type="email" id="email" placeholder="login@pitang.com"></input>
                     <label >Senha</label>
-                    <input type="password" id="senha" placeholder="**********"></input>
-                    <button className="btnSI" type="submit" >SIGN IN</button>
+                    <input onChange ={this.setPassword} value = {this.state.password} type="password" id="senha" placeholder="**********"></input>
+                    <button className="btnSI" type="submit" onClick = {this.submitlistener}>SIGN IN</button>
                     <button className="btnSU" type="submit" >SIGN UP</button>
 
                    
