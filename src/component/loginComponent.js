fc8dad69ab6f48e0b965b7e8d79130764ac074ae
Renan <you@example.com>
2020-03-13 21:15:33 -0300

    inclusao de classe login ao state

--- src/component/loginComponent.js
+++ src/component/loginComponent.js
@@ -1,76 +1,95 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 
 import './LoginStyle.css'
 import logopitang from '../assets/logopitang.PNG'
 
-class LoginComponent extends Component{
+class LoginState {
+    constructor(data) {
+        this.email = '';
+        this.password = '';
+        if(data){
+            this.email = data.email;
+            this.password = data.password;
+        }
 
-    constructor(){
+    }
+}
+class LoginComponent extends Component {
+
+    constructor() {
         super();
-        this.state = {
-            email :'',
-            password : '',
-           
-            
-    
-        };
-      // this.setEmail = this.setEmail.bind(this);  //pergunta a rubens o pq quando faz function normal ele pede q passe no constructor e com bind(this)
-      
-
+        this.state = new LoginState();
+        //this.setPassword = this.setPassword.bind(this);  
 
     }
 
-   
+
     setEmail = event => {
-        this.setState({email:event.target.value});
+        this.state.email = event.target.value;
+        this.setState(new LoginState(this.state));
     }
     /*
     Modo antigo que eu estava fazendo
     setEmail(evento){
         this.setState({email:evento.target.value}); 
       }
+      form.login = e.target.value
     */
-      setPassword = event => {
-        this.setState({password:event.target.value});
+   setPassword = event => {
+    this.state.password = event.target.value;
+    this.setState(new LoginState(this.state));
+}
+   /*
+    setPassword(event) {
+        this.state.password = event.target.value;
+        this.setState(this.state.copy);
     }
-
+*/
     submitlistener = event => {
         alert("Form de login  Email : " + this.state.email + " Password : " + this.state.password);
     }
 
 
-render(){
+    render() {
 
 
-    return(
-        <>
+        return (
+            <>
 
-           <p>{this.state.email}</p>
-           <p>{this.state.password}</p>
-      
-        <div className="container">
-            <img src={logopitang}  alt="LogoPitang"></img>
-           
-            <div className="combo">
-                <p><strong>SIGN IN</strong></p>
+                <p>{this.state.email}</p>
+                <p>{this.state.password}</p>
 
-                <form>
-                    <label >Login</label>
-                    <input onChange ={this.setEmail} value = {this.state.email} type="email" id="email" placeholder="login@pitang.com"></input>
-                    <label >Senha</label>
-                    <input onChange ={this.setPassword} value = {this.state.password} type="password" id="senha" placeholder="**********"></input>
-                    <button className="btnSI" type="submit" onClick = {this.submitlistener}>SIGN IN</button>
-                    <button className="btnSU" type="submit" >SIGN UP</button>
+                <div className="container">
+                    <img src={logopitang} alt="LogoPitang"></img>
 
-                   
-                </form>
+                    <div className="combo">
+                        <p><strong>SIGN IN</strong></p>
 
-            </div>
-        </div>
-        </>
-    );
+                        <form onSubmit={this.submitlistener}>
+                            <label >Login</label>
+                            <input onChange={this.setEmail} value={this.state.email} type="email" id="email" placeholder="login@pitang.com"></input>
+                            <label >Senha</label>
+                            <input onChange={this.setPassword} value={this.state.password} type="password" id="senha" placeholder="**********"></input>
+                           <div>
+                            <div class="form-check">
+                              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                           
+                              <label className="form-check-label" hmlfor="exampleCheck1">Remember Me</label>
+                           
+                            </div>
+                            </div>
+                            <button className="btnSI" type="submit">SIGN IN</button>
+                            <button className="btnSU" type="submit" >SIGN UP</button>
 
-}
+
+                        </form>
+
+                    </div>
+                </div>
+            </>
+        );
+
+    }
 
 }
 export default LoginComponent;
